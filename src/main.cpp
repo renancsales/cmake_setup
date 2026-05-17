fb98178e47780bb0d48c6b63f093eb4d4c2d59fe
renancsales <eng.emp.renan@gmail.com>
2026-05-17 20:33:45 -0300

    Adding raylib

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <raylib.h>
 
 int main(int argc, const char * argv[])
 {
     std::cout << "resources_path=" << RESOURCES_PATH << std::endl;
     std::cout << "Hello, World!\n";
+
+    SetTargetFPS(30);
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(800, 600, "Raylib Test");
+    int posX = 30, posY=30;
+    int size = 100;
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        DrawRectangle(posX, posY, size,size,BLUE);
+        DrawText("Hi!!!", 190, 200, 30, RED);
+        posX+=1;
+
+        EndDrawing();
+    }
     return 0;
 }
