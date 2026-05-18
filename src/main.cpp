a9cf11783114b3a7d3188656ea59061edfd10a5a
renancsales <eng.emp.renan@gmail.com>
2026-05-17 21:12:56 -0300

    Adding imgui into the project

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <raylib.h>
 
+#include <imgui.h>
+#include <rlImGui.h>
+
 int main(int argc, const char * argv[])
 {
     std::cout << "resources_path=" << RESOURCES_PATH << std::endl;
@@ -9,6 +12,8 @@ int main(int argc, const char * argv[])
     SetTargetFPS(30);
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(800, 600, "Raylib Test");
+
+    rlImGuiSetup(true);
     int posX = 30, posY=30;
     int size = 100;
 
@@ -16,12 +21,25 @@ int main(int argc, const char * argv[])
     {
         BeginDrawing();
         ClearBackground(BLACK);
+        rlImGuiBegin();
 
         DrawRectangle(posX, posY, size,size,BLUE);
         DrawText("Hi!!!", 190, 200, 30, RED);
         posX+=1;
 
+        // Testing imgui
+        ImGui::Begin("Settings");
+        ImGui::Text("hello");
+        ImGui::Button("button");
+        ImGui::End();
+
+
+        rlImGuiEnd();
         EndDrawing();
     }
+
+    rlImGuiShutdown();
+    CloseWindow();
+
     return 0;
 }
